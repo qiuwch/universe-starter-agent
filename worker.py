9c2457a50133ecaa97793318558e8186a3ef6895
Weichao Qiu <qiuwch@gmail.com>
2017-05-01 00:57:37 -0400

    Add ccvl cluster spec.

--- worker.py
+++ worker.py
@@ -10,32 +10,14 @@ import os
 from a3c import A3C
 from envs import create_env
 import distutils.version
+import ccvl_cluster_spec
+
 use_tf12_api = distutils.version.LooseVersion(tf.VERSION) >= distutils.version.LooseVersion('0.12.0')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def ccvl_cluster_spec(num_workers, num_ps):
-    """
-More tensorflow setup for data parallelism
-"""
-    cluster = {}
-    port = 12222
-
-    all_ps = []
-    host = 'ccvl2.ccvl.jhu.edu'
-    for _ in range(num_ps):
-        all_ps.append('{}:{}'.format(host, port))
-        port += 1
-    cluster['ps'] = all_ps
-
-    all_workers = [
-       'ccvl1.ccvl.jhu.edu:{port}'.format(port=port),
-       'ccvl3.ccvl.jhu.edu:{port}'.format(port=port),
-    ]
-    cluster['worker'] = all_workers
-    return cluster
 
 # Disables write_meta_graph argument, which freezes entire process and is mostly useless.
 class FastSaver(tf.train.Saver):
@@ -150,7 +132,9 @@ Setting up Tensorflow for data parallel work
                         help="Visualise the gym environment by running env.render() between each timestep")
 
     args = parser.parse_args()
-    spec = ccvl_cluster_spec(args.num_workers, 1)
+    # spec = ccvl_cluster_spec(args.num_workers, 1)
+    spec = ccvl_cluster_spec.five_learners()
+
     cluster = tf.train.ClusterSpec(spec).as_cluster_def()
 
     def shutdown(signal, frame):
@@ -172,5 +156,3 @@ Setting up Tensorflow for data parallel work
 
 if __name__ == "__main__":
     tf.app.run()
-
-
